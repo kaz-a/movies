9018e3cdb429c96dd12c0d7bd19bc50d8dff3e9d
kaz-a <zakscloset@gmail.com>
2018-03-06 23:42:48 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+  app = express(),
+  chalk = require('chalk'),
+  path = require('path'),
+  port = process.env.PORT || 3030;
+
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+app.listen(port, () => {
+  console.log(chalk.yellow(`server listening on port ${port}...`));
+})
