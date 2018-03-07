5fd62f66199ab07069d6af69e8a88262c45d60b6
kaz-a <zakscloset@gmail.com>
2018-03-07 01:15:50 -0500

    load data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import reactDOM from 'react-dom';
+import * as d3 from 'd3';
 
-const Main = () => {
-  return (
-    <div className="container">
-      <h1>helloooooo</h1>
-    </div>
-  )
+class Main extends Component {
+  constructor(){
+    super()
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount(){
+    d3.csv('/data/Copy of movies.csv', (err, data) => {
+      if(err) throw err;
+      console.log('data:', data)
+      this.setState({ data })
+    })
+  }
+
+  render(){
+    const { data } = this.state;
+
+    return (
+      <div className="container">
+        <h1>helloooooo</h1>
+      </div>
+    )
+  }
+  
+  
 }
 
 export default Main;
