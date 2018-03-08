92b36eab3116acc80d38032a9ce89b2993a34151
kaz-a <zakscloset@gmail.com>
2018-03-07 23:44:29 -0500

    linechart

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -4,70 +4,146 @@ import * as d3 from 'd3';
 
 class LineChart extends Component {
   constructor(){
-    super()
-    this.drawLine = this.drawLine.bind(this)
+    super();
+    this.drawLines = this.drawLines.bind(this);
   }
 
-  drawLine(){
-    const { grossByDate } = this.props;
-
-    
-    const margin = { top: 20, right: 20, bottom: 100, left: 70 };
-    const width = 600 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
-    const svg = d3.select(".linechart").append("svg").attr("class", "linechart")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    
-    const g = svg.append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    const x = d3.scaleTime().range([0, width]),
-      y = d3.scaleLinear().range([height, 0]); 
-
-    console.log('grossByDate', grossByDate)
-   
-    var line = d3.line()
-      .x(d => x(d.key))
-      .y(d => y(d.value.openingSum))
-
-    x.domain(d3.extent(grossByDate, d => d.key));
-    y.domain(d3.extent(grossByDate, d => d.value.openingSum));
-
-    g.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-      .select(".domain")
-      .remove();
-
-    g.append("g")
-      .call(d3.axisLeft(y))
-      .append("text")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Price ($)");
-
-    g.append("path")
-      .datum(grossByDate)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("stroke-width", 1.5)
-      .attr("d", line);
-
-
+  componentDidMount(){
+    this.drawLines();
   }
+
+  drawLines(){
+    d3.csv('/data/gross_by_date.csv', (err, data) => {
+      if(err) throw err;
+
+      data.forEach(d => {
+        d.id = d[''];
+        d.openDate = new Date(d.open_date);
+        d.openingGross = +d.opening_gross;
+        d.totalGross = +d.total_gross;
+      })
+      // console.log('line chart data:', data);
+
+      const dateFormat = d3.timeFormat("%b %d, %Y")
+
+      const margin = { top: 20, right: 50, bottom: 50, left: 100 };
+      const width = 700 - margin.left - margin.right, 
+        height = 400 - margin.top - margin.bottom;
+
+      const svg = d3.select(".linechart").append("svg").attr("class", "linechart")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+      
+      const g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      const tooltip = d3.select("body").append("div")   
+        .attr("class", "tooltip")               
+        .style("opacity", 0);
+
+      const x = d3.scaleTime().range([0, width]),
+        y = d3.scaleLinear().range([height, 0]); 
+
+      const min = d3.min(data, d => d.totalGross),
+        max = d3.max(data, d => d.totalGross);
+
+      const xExtent = d3.extent(data, d => d.openDate),
+        yExtent = d3.extent(data, d => d.totalGross);
+
+      x.domain(xExtent);
+      y.domain(d3.extent([0, max]));
+
+      const totalLine = d3.line()
+        .x(d => x(d.openDate))
+        .y(d => y(d.totalGross));
+
+      const openingLine = d3.line()
+        .x(d => x(d.openDate))
+        .y(d => y(d.openingGross));
+
+      g.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x)
+        .ticks(6).tickFormat(d3.timeFormat("%m/%d/%y")))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("x", width/2)
+        .attr("dy", "3em")
+        .attr("text-anchor", "middle")
+        .text("Open Date");
+    
+      g.append("g")
+        .attr("class", "y-axis")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .html("Gross ($)");
+
+      g.append("path")
+        .datum(data)
+        .attr("class", "linechart-total")
+        .attr("fill", "none")
+        .attr("stroke", "#3ebdb2")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", totalLine);
+
+      g.append("path")
+        .datum(data)
+        .attr("class", "linechart-opening")
+        .attr("fill", "none")
+        .attr("stroke", "#C6BB8C")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", openingLine);
+
+      g.selectAll("circle")
+        .data(data)
+        .enter().append("circle")
+        .filter(d => d.totalGross >= 600000000)
+        .attr("class", "linechart-circle")
+        .attr("r", 10)
+        .attr("cx", d => x(d.openDate))
+        .attr("cy", d => y(d.totalGross))  
+        .attr("fill", "#3ebdb2")
+        .attr("stroke", "#3ebdb2")
+        .on("mouseover", function(d) {
+          d3.select(this).transition().ease(d3.easeCubicInOut)
+            .duration(500).style("stroke-width", "1em")
+          tooltip.html(`<span>${dateFormat(d.openDate)}</span><br />Total gross: $${Math.round(d.totalGross/1000000)}M`)
+            .style("opacity", 0.7)
+            .style("left", (d3.event.pageX)+0 + "px") 
+            .style("top", (d3.event.pageY)-0 + "px");
+        })
+        .on("mouseout", function(d) {
+          tooltip.style("opacity", 0);
+          d3.select(this).transition().ease(d3.easeCubicInOut)
+            .duration(500).style("stroke-width", 0);
+        })
+        .on("click", d => {
+          console.log('clicked!')
+        }); 
+        
+
+    })
+  }
+
 
   render(){
     return (
-      <div className="linechart">
-        <h3>How has the movie business been in general over the last 3 years?</h3>
-        { this.drawLine() }
-
+      <div className="row">
+        <div className="col-md-4 text">
+          <h3>How was the movie business in general between 2014 and 2016?</h3>
+          <p>It seems like the movie sales peaked in June and December. Click on a circle to show which movies/genres were doing well on these high peak days.</p>
+        </div> 
+        <div className="col-md-8 linechart"></div>
       </div>
     )
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,11 @@ import LineChart from './LineChart';
 class Main extends Component {
   constructor(){
     super()
-    this.state = { grossByDate: [], dataByOpening$: [], dataByTotal$: [] }
+    this.state = { 
+      data: [], 
+      // grossByDate: [], 
+      // dataByOpening$: [], dataByTotal$: [] 
+    }
   }
   componentDidMount(){
     d3.csv('/data/movies.csv', (err, data) => {
@@ -15,38 +19,27 @@ class Main extends Component {
 
       data.forEach(d => {
         d.id = +d[''];
-        d.openDate = d.open_date
+        d.openDate = new Date(d.open_date)
         d.openingGross = +d.opening_gross.replace(/\$|,/g, '').trim();
         d.theaters = +d.theaters;
         d.totalGross = +d.total_gross.replace(/\$|,/g, '').trim();
       })
 
-      console.log('data:', data)
-
-      const grossByDate = d3.nest().key(d => d.openDate)
-        .rollup(d => {
-          return {
-            openingSum: d3.sum(d, e => e.openingGross),
-            totalSum: d3.sum(d, e => e.totalGross)
-          }
-        })
-        .entries(data)
-       
 
       // const dataByDateAndStudio = d3.nest().key(d => d.openDate).key(d => d.studio).entries(data)
-      const dataByOpening$ = d3.nest().key(d => d.openingGross).entries(data);
-      const dataByTotal$ = d3.nest().key(d => d.totalGross).entries(data);
+      // const dataByOpening$ = d3.nest().key(d => d.openingGross).entries(data);
+      // const dataByTotal$ = d3.nest().key(d => d.totalGross).entries(data);
 
-      this.setState({ grossByDate, dataByOpening$, dataByTotal$ })
+      this.setState({ data })
     })
   }
 
   render(){
-    const { grossByDate, dataByOpening$, dataByTotal$ } = this.state;
+    const { data } = this.state;
 
     return (
       <div className="container">
-        <LineChart grossByDate={ grossByDate } />
+        <LineChart />
       </div>
     )
   }
